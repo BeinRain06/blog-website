f57d3ba17809d552a4c29327731cd2f98be4d0d7
Beinrain06 <elngoueni@gmail.com>
2023-03-23 14:49:37 -0400

    article-box

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,24 +1,24 @@
 import React from "react";
+import logo from "../logo.svg";
 import "./BlogPage.css";
 
 function BlogPage() {
   return (
     <div className="blog_container">
       <div className="blog_content">
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
-        <div className="box"></div>
+        <div className="box">
+          <div className="upper_box">
+            <img src={logo} className="blog_img" alt="error blogpage" />
+            <h3 className="title_item">Aquatica Area</h3>
+            <div className="description_wrap">
+              <p className="description_article">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe,
+                similique.
+              </p>
+            </div>
+          </div>
+          <span className="lower_box">Meridian</span>
+        </div>
       </div>
     </div>
   );
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,9 +7,9 @@ function HomePage() {
       <div className="home_container">
         <div className="home_left_door">
           <div className="home_left">
-            <h3 className="left_home_title">
+            <h4 className="left_home_title">
               BoobyFist Entertainments and Informations
-            </h3>
+            </h4>
             <div className="visit">
               <p className="our_inside">
                 You may like articles articles about Sports, Tech, Foods,
@@ -24,7 +24,7 @@ function HomePage() {
             <div>
               <div className="login_right">
                 <div className="right_poster">image poster</div>
-                <div className="goo_login">Google Login</div>
+                <button className="goo_login">Google Login</button>
               </div>
             </div>
             <div className="circle_2"></div>
